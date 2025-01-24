038965dc197b92314c24dbc8da2abc07751a09c5
Rocío Carmona Torregrosa <rcartor0410@g.educaand.es>
2025-01-24 11:10:40 +0100

    Add files via upload

---
+++ Actividad 3/js/script.js
@@ -0,0 +1,7 @@
+function PedirDatos() {
+    let nombre;
+    let edad;
+    nombre = prompt("Dime tu nombre");
+    edad = prompt("y tu edad");
+    alert("Hola " + nombre + " y veo que tienes" + edad + " años");
+}
