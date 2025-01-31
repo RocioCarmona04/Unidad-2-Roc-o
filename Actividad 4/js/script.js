640174049f0f3770eb7cfa743faf2079b3187b7e
Rocío Carmona Torregrosa <rcartor0410@g.educaand.es>
2025-01-31 11:31:13 +0100

    Add files via upload

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,8 @@
+function prueba() {
+    let a;
+    let b;
+    a= parseInt(prompt("Dime el numero a"));
+    b= parseInt(prompt("Dime el numero b"));
+    alert("El primer numero es: " + (a) + "\nEl segundo numero es: " + (b) + "\nLa suma es: " + (a + b) + "\nLa resta es: " + (a - b) + "\nLa multiplicacion es: " + (a  * b) + "\nLa division es: " + (a / b) + "\nEl resto es: " + (a % b)) 
+
+}
