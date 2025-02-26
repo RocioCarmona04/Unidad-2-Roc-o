6e3af327dc2567bd68574c4847f246453ae2ded0
Rocío Carmona Torregrosa <rcartor0410@g.educaand.es>
2025-02-26 21:23:15 +0100

    Delete actividad 9 directory

--- actividad 9/js/script.js
+++
@@ -1,8 +0,0 @@
-function Factorial() {
-    let resultado = 1;
-    let numero = parseInt(document.getElementById("espacio").value);
-    for (let interacion = 1; interacion<= numero; interacion = interacion + 1);{
-      resultado = resultado * interacion;
-    }
-    document.getElementById("resultado").innerHTML = "El factorial de " + numero + "es " + resultado;
-    }
